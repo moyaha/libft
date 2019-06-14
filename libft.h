387e26fc6a750d4edee4275c26e6c815d505e6cf
Precious MOYAHA <pmoyaha@c4r4s4.wethinkcode.co.za>
2019-06-14 15:15:40 +0200

    ft_strjoin.c

--- libft.h
+++ libft.h
@@ -6,18 +6,16 @@
 /*   By: pmoyaha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 23:15:55 by pmoyaha           #+#    #+#             */
-/*   Updated: 2019/06/14 11:17:24 by pmoyaha          ###   ########.fr       */
+/*   Updated: 2019/06/14 15:12:46 by pmoyaha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include  <unistd.h>
-# include  <stdlib.h>
-# include  <string.h>
-# include  <stdio.h>
-# include  <ctype.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
 
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -29,7 +27,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_memcmp(const void *s1, const void*s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strdup(const char *s1);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
