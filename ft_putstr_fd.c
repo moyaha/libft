ca8e04078286dd587537d6a62ec975a2467b87a0
Precious MOYAHA <pmoyaha@c4r4s4.wethinkcode.co.za>
2019-06-19 15:41:58 +0200

    ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,15 +6,15 @@
 /*   By: pmoyaha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 09:26:57 by pmoyaha           #+#    #+#             */
-/*   Updated: 2019/06/07 10:52:22 by pmoyaha          ###   ########.fr       */
+/*   Updated: 2019/06/18 15:02:20 by pmoyaha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr_fd(char const *s,int fd)
+void	ft_putstr_fd(char const *s, int fd)
 {
-	if(!s)
+	if (!s)
 	{
 		return ;
 	}
