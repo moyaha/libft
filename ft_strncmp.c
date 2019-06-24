7ae04bafe44316d32f67fdd5cf9f9c53d656aa2e
Precious MOYAHA <pmoyaha@c4r4s4.wethinkcode.co.za>
2019-06-24 09:41:33 +0200

    ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pmoyaha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 02:17:30 by pmoyaha           #+#    #+#             */
-/*   Updated: 2019/06/22 01:26:56 by pmoyaha          ###   ########.fr       */
+/*   Updated: 2019/06/24 09:32:47 by pmoyaha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char	*str2;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
